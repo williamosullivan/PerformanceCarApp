cf9f1f6db6ee0459e8f9d50dd79c409a55feb5ce
a2davidr <roedavidc@yahoo.com>
2014-08-05 15:27:59 -0400

    CSS Update 1

--- PerformanceCarApp/App_Start/BundleConfig.cs
+++ PerformanceCarApp/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace PerformanceCarApp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/Style.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
