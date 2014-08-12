77c73b655cccdee71395be7e08f2dc9fcbf7aa82
ltigue <moneynloans@yahoo.com>
2014-08-12 15:35:03 -0400

    added a using System
    added system.io.path to user

--- PerformanceCarApp/Models/User.cs
+++ PerformanceCarApp/Models/User.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.IO.Path;
 
 namespace PerformanceCarApp.Models
 {
