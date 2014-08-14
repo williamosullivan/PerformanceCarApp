5ca723e6db34568a4f2a349c6c4a81eb8c62b2c7
ltigue <moneynloans@yahoo.com>
2014-08-14 14:54:44 -0400

    new photo- config error correction
    changes to background

--- PerformanceCarApp/Migrations/Configuration.cs
+++ PerformanceCarApp/Migrations/Configuration.cs
@@ -11,7 +11,7 @@ namespace PerformanceCarApp.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(PerformanceCarApp.DAL.CarContext context)
